7905f604202d4f7cc88a8a329d5651b368ef8a27
KACIMI Jugurtha <>
2020-11-30 13:37:16 +0100

    Navbar coded and responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import menu from '../assets/icons/menu.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Handelp</h1>
+      <div className="navbar__title">
+        <img
+          src={menu}
+          alt="menu"
+          width="30px"
+          height="30px"
+          className="d-md-none d-block"
+        />
+        <h1>Handelp</h1>
+      </div>
       <div className="navbar__links">
-        <Link to="/">A propos</Link>
-        <Link to="/">Services</Link>
-        <Link to="/">Store</Link>
+        <div className="navbar__navLinks d-md-flex d-none">
+          <Link to="/">A propos</Link>
+          <Link to="/">Services</Link>
+          <Link to="/">Store</Link>
+        </div>
         <div className="navbar__authLinks">
-          <Link to="/">Connexion</Link>
+          <Link to="/" className="d-sm-inline d-none">
+            Connexion
+          </Link>
           <Link to="/" className="navbar__linkRegister">
             Inscription
           </Link>
