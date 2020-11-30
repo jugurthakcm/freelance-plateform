67e5d67cc532fa7a9f8ffc0d174be11112619929
KACIMI Jugurtha <>
2020-11-30 13:12:24 +0100

    Navbar coded

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="navbar">
+      <h1>Handelp</h1>
+      <div className="navbar__links">
+        <Link to="/">A propos</Link>
+        <Link to="/">Services</Link>
+        <Link to="/">Store</Link>
+        <div className="navbar__authLinks">
+          <Link to="/">Connexion</Link>
+          <Link to="/" className="navbar__linkRegister">
+            Inscription
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
