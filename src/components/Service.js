abc07a1184d62a45d079a5502b0129ced9fd13a2
KACIMI Jugurtha <>
2020-12-01 14:34:53 +0100

    Adding onClick to service

--- src/components/Service.js
+++ src/components/Service.js
@@ -6,9 +6,16 @@ const Service = ({ title, background }) => {
     e.target.innerHTML =
       '<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dolore quiincidunt provident deleniti omnis alias consequatur est fugit voluptas.</p>';
   };
+
   const handleMouseLeave = (e) => {
     e.target.innerHTML = `<h2>${title}</h2>`;
   };
+
+  const handleClick = (e) => {
+    e.target.innerHTML =
+      '<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dolore quiincidunt provident deleniti omnis alias consequatur est fugit voluptas.</p>';
+  };
+
   return (
     <div className="service__container col-lg-3 col-md-4 col-sm-6 p-2">
       <div
@@ -16,6 +23,7 @@ const Service = ({ title, background }) => {
         style={{ backgroundColor: background }}
         onMouseOver={handleMouseOver}
         onMouseLeave={handleMouseLeave}
+        onClick={handleClick}
       >
         <h2>{title}</h2>
       </div>
