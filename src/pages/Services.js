d2a8769581f8a761abe5e1622cf116921f0fa82e
jugurthakcm <kjugurtha1711@gmail.com>
2020-12-28 13:57:59 +0100

    Adding services images

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,25 +3,37 @@ import './Services.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 // import { Link } from 'react-router-dom';
+import programmation from '../assets/images/programmation.jpg';
+import digitalMarketing from '../assets/images/digitalMarketing.jpg';
+import photography from '../assets/images/photography.jpg';
+import translation from '../assets/images/translation.jpg';
+import business from '../assets/images/business.jpg';
+import design from '../assets/images/design.jpeg';
+import motion from '../assets/images/motion.jpeg';
+import voice from '../assets/images/voice.jpg';
 
 const Services = () => {
   const services = [
     {
       title: 'Programmation et Développement',
-      background: '#0a043c',
+      background: programmation,
       link: 'programmation',
     },
-    { title: 'Business', background: '#583d72', link: 'business' },
-    { title: 'Marketing', background: '#682c0e', link: 'marketing' },
-    { title: 'Coaching', background: '#222831', link: 'coaching' },
+    { title: 'Business', background: business, link: 'business' },
+    { title: 'Marketing', background: digitalMarketing, link: 'marketing' },
+    { title: 'Photography', background: photography, link: 'coaching' },
     {
       title: 'Traduction et Rédaction',
-      background: '#16697a',
+      background: translation,
       link: 'translation_redaction',
     },
-    { title: 'Voice', background: '#222831', link: 'voice' },
-    { title: 'Design', background: '#393e46', link: 'design' },
-    { title: 'Montage Vidéo', background: '#ea2c62', link: 'graphic_motion' },
+    { title: 'Voice', background: voice, link: 'voice' },
+    { title: 'Design', background: design, link: 'design' },
+    {
+      title: 'Montage Vidéo',
+      background: motion,
+      link: 'graphic_motion',
+    },
   ];
   return (
     <>
@@ -47,10 +59,9 @@ const Services = () => {
             <div className="service__container col-lg-3 col-md-4 col-sm-6 p-2">
               <div
                 className="service"
-                style={{ backgroundColor: service.background }}
-              >
-                <h2>{service.title}</h2>
-              </div>
+                style={{ backgroundImage: `url(${service.background})` }}
+              ></div>
+              <h2>{service.title}</h2>
             </div>
           ))}
         </div>
