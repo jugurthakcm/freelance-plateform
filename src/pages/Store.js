e3b8264eb0d60f82dceeb214fe3647931bc0d16f
jugurthakcm <kjugurtha1711@gmail.com>
2020-12-29 19:38:29 +0100

    Store designed

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -2,16 +2,40 @@ import React from 'react';
 import './Store.css';
 import Navbar from '../components/Navbar';
 import StackGrid from 'react-stack-grid';
+import { store } from '../data/store';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const Store = () => {
   return (
     <>
       <Navbar />
-      <StackGrid columnWidth={150}>
-        <div key="key1">Item 1</div>
-        <div key="key2">Item 2</div>
-        <div key="key3">Item 3</div>
-      </StackGrid>
+      <div className="store">
+        <StackGrid columnWidth={275} gutterWidth={20} gutterHeight={20}>
+          {store.map((gig) => (
+            <div className="store__gig">
+              <div
+                className="gig__image"
+                style={{
+                  backgroundColor: '#' + gig.color,
+                  height: gig.height + 'px',
+                }}
+              ></div>
+              <div className="gig__details mx-3 my-3">
+                <p className="gig__type">{gig.type}</p>
+                <h5 className="gig__title">{gig.title}</h5>
+                <p className="gig__seller">by {gig.seller}</p>
+                <div className="gig__footer">
+                  <button>Contact the seller</button>
+                  <span>
+                    <FontAwesomeIcon icon={faHeart} /> {gig.likes}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </StackGrid>
+      </div>
     </>
   );
 };
