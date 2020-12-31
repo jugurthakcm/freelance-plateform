e01f95e42d15050e4148743c23c980ca6946ea9b
jugurthakcm <kjugurtha1711@gmail.com>
2020-12-31 07:04:59 +0100

    Fixing typo in description text

--- src/i18n/content/english.js
+++ src/i18n/content/english.js
@@ -4,15 +4,16 @@ const englishContent = {
   [LOCALES.ENGLISH]: {
     description: {
       descriptionTitle: 'Who we are ?',
-      descriptionText: `<strong>Handelp</strong> is your number 1 partner website in Algeria .when looking for professionals in the field of communication ,creation and marketing ,design and graphic arts as well as computer engineering;.
+      descriptionText: `<strong>Handelp</strong> is your number 1 partner website in Algeria when looking for professionals in the field of communication, creation and marketing, design and graphic arts as well as computer engineering.
       <br></br>
       <br></br>
-      Companies and individuals often encounter problems during the development of projects to a lack of stuff or professionals in a specific field,      <br></br>
-      <br></br>
-      <strong>Handelp </strong> will be there to help you find the ideal person who will be able to develop your idea or project thanks to our large address books and contacts; all you have to do id contact us on our website telling us your needs ;
+      Companies and individuals often encounter problems during the development of projects to a lack of stuff or professionals in a specific field.      
       <br></br>
       <br></br>
-      We will take care of analyzing your needs then you can choose the professional who are suitable to your needs and your budget `,
+      <strong>Handelp</strong> will be there to help you find the ideal person who will be able to develop your idea or project, thanks to our large address books and contacts; all you have to do is to contact us on our website telling us your needs ;
+      <br></br>
+      <br></br>
+      We will take care of analyzing your needs then you can choose the professional who are suitable to your needs and your budget. `,
     },
     navbar: {
       home: 'Home',
--- src/i18n/content/french.js
+++ src/i18n/content/french.js
@@ -10,7 +10,7 @@ const frenchContent = {
       Les entreprises et les particuliers rencontrent souvent des problèmes lors du développement des projets par manque d’effectif ou de professionnels d’un domaine précis, <strong>Handelp </strong> sera là pour vous aider à trouver la personne idéale qui saura mettre au point votre idée ou projet grâce à son large carnet d’adresses et de contacts.
       <br></br>
       <br></br>
-      Vous n’aurez qu’à nous contacter via un message sur notre site web en joignant vos besoins et votre contact, nous prendrons le soin    d’analyser vos besoins pour vous suggérer les professionnels qui conviennent à votre projet.`,
+      Vous n’aurez qu’à nous contacter via un message sur notre site web en joignant vos besoins et votre contact, nous prendrons le soin d’analyser vos besoins pour vous suggérer les professionnels qui conviennent à votre projet.`,
     },
     navbar: {
       home: 'Acceuil',
