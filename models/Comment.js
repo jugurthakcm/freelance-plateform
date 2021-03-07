20c9ffb306d40ff57e56f428cb6748c5aab6c97d
jugurthakcm <kjugurtha1711@gmail.com>
2021-03-07 19:38:05 +0100

    Add comment model - BACKEND

---
+++ models/Comment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    min: 1,
+  },
+  senderId: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 50,
+  },
+  receiverId: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 50,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+exports.Comment = mongoose.model('comments', commentSchema);
