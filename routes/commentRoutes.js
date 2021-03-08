89b83343fcb0c07724ac653e547b150d3f4cbe8e
jugurthakcm <kjugurtha1711@gmail.com>
2021-03-08 12:05:07 +0100

    Adding delete comment route

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addComment, editComment } = require('../controllers/commentController');
+const {
+  addComment,
+  editComment,
+  deleteComment,
+} = require('../controllers/commentController');
 const { auth } = require('../middlewares/authMiddleware');
 
 router.post('/add', auth, addComment);
 router.put('/edit', auth, editComment);
+router.delete('/delete', auth, deleteComment);
 module.exports = router;
